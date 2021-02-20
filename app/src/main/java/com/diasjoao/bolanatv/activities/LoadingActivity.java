5bebb7d684ab88b95fe18aa8e17b06432defcc30
João Dias <joao.dias1397@gmail.com>
2021-02-20 11:26:22 +0000

    Fix Loading screen no network connection

--- app/src/main/java/com/diasjoao/bolanatv/activities/LoadingActivity.java
+++ app/src/main/java/com/diasjoao/bolanatv/activities/LoadingActivity.java
@@ -14,6 +14,7 @@ import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -23,6 +24,7 @@ import com.android.volley.toolbox.Volley;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.w3c.dom.Text;
 
 import java.io.Serializable;
 import java.text.ParseException;
@@ -41,20 +43,29 @@ public class LoadingActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loading);
 
-        ImageView logo = findViewById(R.id.logo);
-        Button repetir = findViewById(R.id.repetir);
-        repetir.setVisibility(View.GONE);
+        TextView retryText = findViewById(R.id.retryText);
+        retryText.setVisibility(View.GONE);
+        Button retry = findViewById(R.id.retry);
+        retry.setVisibility(View.GONE);
 
+        ImageView logo = findViewById(R.id.logo);
         Animation animation = AnimationUtils.loadAnimation(LoadingActivity.this, R.anim.pulse);
         logo.startAnimation(animation);
+
+        TextView loadingText = findViewById(R.id.loading);
+        loadingText.setVisibility(View.VISIBLE);
 
         requestQueue = Volley.newRequestQueue(this);
 
         if (NetworkUtils.isNetworkConnected(this)) {
             jsonParse();
         } else {
-            repetir.setVisibility(View.VISIBLE);
-            repetir.setOnClickListener(view -> {
+            logo.clearAnimation();
+            loadingText.setVisibility(View.GONE);
+
+            retryText.setVisibility(View.VISIBLE);
+            retry.setVisibility(View.VISIBLE);
+            retry.setOnClickListener(view -> {
                 finish();
                 startActivity(getIntent());
             });
